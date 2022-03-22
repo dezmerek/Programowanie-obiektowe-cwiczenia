36776537d85e94842f495c7091421f9dab0a33a9
Mateusz Szymański <mszymanski027@gmail.com>
2022-03-22 21:33:58 +0100

    cw laby

---
+++ Różnice kwadratowe/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace Różnice_kwadratowe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            static void QuadraticEquation(int a, int b, int c)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
