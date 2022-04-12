463904d267d990940da59537ac69a5bb969a9549
Mateusz Szymański <mszymanski027@gmail.com>
2022-04-13 00:48:27 +0200

    add roznice kwadratowe

--- Różnice kwadratowe/Program.cs
+++ Różnice kwadratowe/Program.cs
@@ -7,10 +7,49 @@ namespace Różnice_kwadratowe
     {
         static void Main(string[] args)
         {
-            static void QuadraticEquation(int a, int b, int c)
+            // wczytaj dane
+            Console.Write("a:");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("b:");
+            int b = int.Parse(Console.ReadLine());
+            Console.Write("c:");
+            int c = int.Parse(Console.ReadLine());
+
+            static void QuadraticEquation(double a, double b, double c)
             {
-                throw new NotImplementedException();
+                double delta = (b * b) - (4 * a * c);
+
+                if (a == 0 & b == 0 & c == 0)
+                {
+                    Console.WriteLine("infinity");
+                    return;
+                }
+                else if (a == 0 & b == 0)
+                {
+                    Console.WriteLine("empty");
+                    return;
+                };
+
+                if (delta == 0)
+                {
+                    double x0 = -b / (2 * a);
+
+                    Console.WriteLine("x=" + x0 + ".00");
+                }
+                else if (delta < 0)
+                {
+                    Console.WriteLine("empty");
+                }
+                else
+                {
+                    double x1 = (-b - Math.Sqrt(delta)) / (2 * a);
+                    double x2 = (-b + Math.Sqrt(delta)) / (2 * a);
+
+                    Console.WriteLine("x1=" + x1 + ".00");
+                    Console.WriteLine("x2=" + x2 + ".00");
+                }
             }
+            QuadraticEquation(a, b, c);
         }
     }
 }
