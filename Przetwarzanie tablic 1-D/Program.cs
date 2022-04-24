2d8a1e5a03cc312697859f67284a53bb01e584a7
Mateusz Szymański <mszymanski027@gmail.com>
2022-04-24 22:59:25 +0200

    +

---
+++ Przetwarzanie tablic 1-D/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Przetwarzanie_tablic_1_D
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
