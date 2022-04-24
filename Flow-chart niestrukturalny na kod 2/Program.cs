ca063f7820f7ad0f2719b10dc71881ac57c0d6de
Mateusz Szymański <mszymanski027@gmail.com>
2022-04-24 16:23:15 +0200

    add flow 2

---
+++ Flow-chart niestrukturalny na kod 2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Flow_chart_niestrukturalny_na_kod_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //wczytaj dane
+            string wszystkieDane = Console.ReadLine();
+            string[] podzieloneDane = wszystkieDane.Split(' ');
+
+            int x = int.Parse(podzieloneDane[0]);
+            int y = int.Parse(podzieloneDane[1]);
+            int z = int.Parse(podzieloneDane[2]);
+
+            for (; x > 0; ) 
+            { 
+                if(y>0)
+                {
+                    x = x - 1;
+                    y = y - 1;
+                    Console.Write("C");
+                }
+                else
+                {
+                    Console.Write("D");
+                    if(z>0)
+                    {
+                        Console.WriteLine();
+                        return;
+                    }
+                    else
+                    {
+                        Console.Write("G");
+                        Console.WriteLine();
+                        return;
+                    }    
+                }
+            }
+
+            for (; x <= 0;)
+            {
+                Console.Write("E");
+                Console.Write("G");
+                Console.WriteLine();
+                return;
+            }
+        }
+    }
+}
---
+++ Flow-chart niestrukturalny na kod/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Flow_chart_niestrukturalny_na_kod_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // wczytaj dane
+            string wszystkieDane = Console.ReadLine();
+            string[] podzieloneDane = wszystkieDane.Split(' ');
+
+            int x = int.Parse(podzieloneDane[0]);
+            int y = int.Parse(podzieloneDane[1]);
+            int z = int.Parse(podzieloneDane[2]);
+
+            start:
+            if (x > 0)
+            {
+                if(y > 0)
+                {
+                    x = x - 1;
+                    y = y - 1;
+                    Console.Write("C");
+                    goto start;
+                }
+                else
+                {
+                    Console.Write("D");
+                    if(z > 0)
+                    {
+                        Console.WriteLine();
+                        return;
+                    }
+                    else
+                    {
+                        Console.Write("G");
+                        Console.WriteLine();
+                        return;
+                    }
+                }
+            }
+            else
+            {
+                Console.Write("EG");
+                Console.WriteLine();
+                return;
+            }
+        }
+    }
+}
