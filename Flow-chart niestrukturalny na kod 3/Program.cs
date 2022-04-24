a9645ab9732a0f6614b322fa7b4c106fb71d3c98
Mateusz Szymański <mszymanski027@gmail.com>
2022-04-24 16:31:11 +0200

    add flow 3

---
+++ Flow-chart niestrukturalny na kod 3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Flow_chart_niestrukturalny_na_kod_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string wszystkieDane = Console.ReadLine();
+            string[] podzieloneDane = wszystkieDane.Split(' ');
+
+            int x = int.Parse(podzieloneDane[0]);
+            int y = int.Parse(podzieloneDane[1]);
+            int z = int.Parse(podzieloneDane[2]);
+
+            while(x>0)
+            {
+                if(y>0)
+                {
+                    x=x-1;
+                    y=y-1;
+                    Console.Write("C");
+                }
+                else
+                {
+                    Console.Write("D");
+                    if(z>0)
+                    {
+                        Console.WriteLine();
+                        return;
+                    }
+                    else
+                    {
+                        Console.Write("G");
+                        Console.WriteLine();
+                        return;
+                    }
+                }
+            }
+
+            while(x<=0)
+            {
+                Console.Write("E");
+                Console.Write("G");
+                Console.WriteLine();
+                return;
+            }
+        }
+    }
+}
